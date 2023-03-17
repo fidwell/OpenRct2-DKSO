0907c50759c30bbb74b9be55a53571b729ae5e80
Andrew <5436387+fidwell@users.noreply.github.com>
2023-03-16 21:03:21 -0400

    Some json editing

--- dat-convert.ps1
+++ dat-convert.ps1
@@ -21,21 +21,23 @@ $objectName = $identifiers[2]
 $newIdentifier = "$($namespace).$($objectType).$($objectName)"
 $objectPath = ".\objects\$($newIdentifier)"
 $imagePath = "$($objectPath)\images"
+$objectJson = "$($objectPath)\object.json"
 
 function ExtractSprites
 {
+    Write-Host "Creating directories..."
     New-Item -Path "$($objectPath)" -ItemType Directory -Force | Out-Null
     New-Item -Path "$($imagePath)" -ItemType Directory -Force | Out-Null
 
     $command = ""
     if ($customIdentifier.Length -eq 0)
     {
-        Write-Host "Extracting vanilla object"
+        Write-Host "Extracting vanilla object..."
         $command = "$($openRct2com) sprite exportalldat $($rctIdentifier) $($imagePath) > $($objectPath)/images.json"
     }
     else
     {
-        Write-Host "Extracting custom object"
+        Write-Host "Extracting custom object..."
         $command = "$($openRct2com) sprite exportalldat $($customIdentifier) $($imagePath) > $($objectPath)/images.json"
     }
 
@@ -44,16 +46,38 @@ function ExtractSprites
 
 function CopyData
 {
+    Write-Host "Copying json..."
     $source = "$($openRct2bin)\data\object\$($originalGame)\$($objectType)\$($openRct2Identifier).json"
-    
-    Copy-Item $source -Destination "$($objectPath)\object.json"
+    Copy-Item $source -Destination $objectJson
 }
 
 function EditJson
 {
-    # To do
+    Write-Host "Editing json..."
+
+    # Replace id
+	$regex = [regex]::Escape("""id"": ""$($openRct2Identifier)"",")
+	(Get-Content $objectJson) -replace $regex, """id"": ""$($newIdentifier)""," | Set-Content $objectJson
+
+    # Change or remove original id
+    $regex = "\""originalId\"": \"".*"
+	(Get-Content $objectJson) -replace $regex, """originalId"": """"," | Set-Content $objectJson
+
+    # Change source game
+    $regex = "\""sourceGame\"": \[.*"
+	(Get-Content $objectJson) -replace $regex, """sourceGame"": [""custom""]," | Set-Content $objectJson
+    # to do
+
+    # Add recolorable flags
+    # to do
+
+    # Add images
+    # to do
 }
 
 ExtractSprites
 CopyData
 EditJson
+
+Write-Host "Done"
+  
