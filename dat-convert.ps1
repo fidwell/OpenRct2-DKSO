b902397f29a6785e900f87910f9aae491eabe547
Andrew <5436387+fidwell@users.noreply.github.com>
2023-03-16 21:41:52 -0400

    Script refactoring; allow for multiple objects

--- dat-convert.ps1
+++ dat-convert.ps1
@@ -1,9 +1,3 @@
-if ($args.Length -lt 2)
-{
-    Write-Host "Usage: dat-convert RCT-IDENTIFIER openrct2.identifier CUSTOM-IDENTIFIER"
-    return
-}
-
 $rctIdentifier = $args[0]
 $openRct2Identifier = $args[1]
 $customIdentifier = $args[2]
@@ -13,17 +7,12 @@ $openRct2bin = "$($documents)\OpenRct2\bin"
 $openRct2com = "$($openRct2bin)\openrct2.com"
 
 $namespace = "fidwell"
-$identifiers = $openRct2Identifier.Split(".")
-$originalGame = $identifiers[0]
-$objectType = $identifiers[1]
-$objectName = $identifiers[2]
 
-$newIdentifier = "$($namespace).$($objectType).$($objectName)"
-$objectPath = ".\objects\$($newIdentifier)"
-$imagePath = "$($objectPath)\images"
-$objectJson = "$($objectPath)\object.json"
-
-function ExtractSprites
+function ExtractSprites(
+    [string]$objectPath,
+    [string]$imagePath,
+    [string]$customIdentifier,
+    [string]$rctIdentifier)
 {
     Write-Host "Creating directories..."
     New-Item -Path "$($objectPath)" -ItemType Directory -Force | Out-Null
@@ -44,14 +33,22 @@ function ExtractSprites
     Invoke-Expression -Command $command
 }
 
-function CopyData
+function CopyData(
+    [string]$openRct2bin,
+    [string]$originalGame,
+    [string]$objectType,
+    [string]$openRct2Identifier,
+    [string]$objectJson)
 {
     Write-Host "Copying json..."
     $source = "$($openRct2bin)\data\object\$($originalGame)\$($objectType)\$($openRct2Identifier).json"
     Copy-Item $source -Destination $objectJson
 }
 
-function EditJson
+function EditJson(
+    [string]$openRct2Identifier,
+    [string]$objectJson,
+    [string]$newIdentifier)
 {
     Write-Host "Editing json..."
 
@@ -75,9 +72,42 @@ function EditJson
     # to do
 }
 
-ExtractSprites
-CopyData
-EditJson
+function ConvertDat(
+    [string]$rctIdentifier,
+    [string]$openRct2Identifier,
+    [string]$customIdentifier
+)
+{
+    $identifiers = $openRct2Identifier.Split(".")
+    $originalGame = $identifiers[0]
+    $objectType = $identifiers[1]
+    $objectName = $identifiers[2]
 
-Write-Host "Done"
-  
+    $newIdentifier = "$($namespace).$($objectType).$($objectName)"
+    $objectPath = ".\objects\$($newIdentifier)"
+    $imagePath = "$($objectPath)\images"
+    $objectJson = "$($objectPath)\object.json"
+
+    ExtractSprites $objectPath $imagePath $customIdentifier $rctIdentifier
+    CopyData $openRct2bin $originalGame $objectType $openRct2Identifier $objectJson
+    EditJson $openRct2Identifier $objectJson $newIdentifier
+
+    Write-Host "Done"
+}
+
+$objects = @(`
+[System.Tuple]::Create("TIC", "rct2.scenery_small.tic", "GWTRIC"), `
+[System.Tuple]::Create("TLC", "rct2.scenery_small.tlc", "GWTRLC"), `
+[System.Tuple]::Create("TMC", "rct2.scenery_small.tmc", "GWTRMC"), `
+[System.Tuple]::Create("TMP", "rct2.scenery_small.tmp", "GWTRMP"), `
+[System.Tuple]::Create("TITC", "rct2.scenery_small.titc", "GWTRITC"), `
+[System.Tuple]::Create("TGHC", "rct2.scenery_small.tghc", "GWTRGHC"), `
+[System.Tuple]::Create("TAC", "rct2.scenery_small.tac", "GWTRAC"), `
+[System.Tuple]::Create("TGHC2", "rct2.scenery_small.tghc2", "GWTRHT"))
+
+Write-Host "Converting $($objects.length) objects..."
+
+for ($i = 0; $i -lt $objects.length; $i++)
+{
+    ConvertDat $objects[$i].Item1 $objects[$i].Item2 $objects[$i].Item3
+}
