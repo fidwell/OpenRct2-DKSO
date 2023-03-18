1276fd5f06cd33586c804c64658f507bf6711e9f
Andrew <5436387+fidwell@users.noreply.github.com>
2023-03-18 12:28:16 -0400

    Single script to make and install all objects

---
+++ make-all.ps1
@@ -0,0 +1,15 @@
+Write-Host "Zipping groups..."
+. ".\zip-groups.ps1" | Out-Null
+
+Write-Host "Zipping objects..."
+. ".\zip-objects.ps1" | Out-Null
+
+Write-Host "Installing objects in OpenRTC2..."
+
+$homepath = Get-Location
+$dist = "$($homepath)\dist"
+$documents = [Environment]::GetFolderPath("MyDocuments")
+$openRct2object = "$($documents)\OpenRct2\object"
+
+Get-ChildItem -Path $dist -File | Move-Item -Destination $openRct2object -Force
+Write-Host "Done"
