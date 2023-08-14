22971e29d6b43772b93b42bdea9b6e7386a917ca
Andrew <5436387+fidwell@users.noreply.github.com>
2023-08-14 19:33:19 -0400

    Convenient folder for installing via script

--- make-all.ps1
+++ make-all.ps1
@@ -9,7 +9,7 @@ Write-Host "Installing objects in OpenRTC2..."
 $homepath = Get-Location
 $dist = "$($homepath)\dist"
 $documents = [Environment]::GetFolderPath("MyDocuments")
-$openRct2object = "$($documents)\OpenRct2\object"
+$openRct2object = "$($documents)\OpenRct2\object\dkso"
 
 Get-ChildItem -Path $dist -File | Move-Item -Destination $openRct2object -Force
 Write-Host "Done"
